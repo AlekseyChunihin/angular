a1e7d59cf3942900cabbb77f257a51ea43539f3a
headcrabe98 <alexey3322179@gmail.com>
2019-12-17 18:56:42 +0200

    Lab5

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BookService } from '../services/book.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router} from '@angular/router';
 
 @Component({
@@ -8,16 +9,30 @@ import { Router} from '@angular/router';
   styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent implements OnInit {
-	
-  constructor(public bookService:BookService, private _route:Router ) { }
 
-  public addBook(name,author,year){
-  	this.bookService.add(name,author,year);
+    fG:FormGroup;
+    flag=true;
+  constructor(public bookService:BookService, private _route:Router) { }
+
+  public addBook(){
+
+    if(this.fG.valid){
+    this.bookService.add(this.fG.value.nameControl,this.fG.value.authorControl,this.fG.value.yearControl);
     this._route.navigate(['books']);
-
   }
+  else
+  {
+    this.flag=false;
+  }
+  }
+  
 
   ngOnInit() {
+    this.fG = new FormGroup({
+    nameControl: new FormControl('', [Validators.pattern('[A-ZА-Я].*')]),
+    authorControl: new FormControl('', [Validators.required]),
+    yearControl: new FormControl('', [Validators.required])
+  });
   }
 
 }
